a2fbb12cdc8ef98a6fa80f4edcb7f947fd70fe5a
Cole Harris <colenh@protonmail.com>
2024-06-25 19:38:34 -0400

    add wandera, update sailboat games, general fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import Link from "next/link";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
 	return (
-		<div className="dark flex justify-center min-h-screen bg-neutral-900 px-4 sm:px-6 lg:px-8">
-			<div className="flex items-center justify-center h-screen">
+		<div className="dark flex justify-center min-h-[calc(100dvh)] bg-neutral-900 px-4 sm:px-6 lg:px-8">
+			<div className="flex items-center justify-center h-[calc(100dvh)]">
 				<div className="max-w-xl space-y-8">
 					<div className="text-center">
 						<Image
@@ -28,13 +28,19 @@ export default function Home() {
 						<h3 className="text-lg font-semibold text-white">// projects</h3>
 						<ul className="list-disc pl-5 space-y-1 text-neutral-300">
 							<li>
-								<Link className="text-neutral-400 hover:text-neutral-600 opacity-80 transition-colors" href="https://cow.yoga">
+								<Link className="text-neutral-400 hover:text-opacity-70 opacity-80 transition-colors" href="https://cow.yoga">
 									cow yoga
 								</Link>{" "}
 								- a team creating products to simplify daily life for businesses and individuals, one at a time.
 							</li>
 							<li>
-								<Link className="text-[#c4621b] hover:text-[#e99b62] opacity-80 transition-colors" href="https://oranjetaxi.xyz">
+								<Link className="text-[#a4d233] hover:text-opacity-70 opacity-80 transition-colors" href="https://wandera.app">
+									wandera
+								</Link>{" "}
+								- enables you to control and amend your Roblox datastores from any place, device or planet regardless of your access to Studio.
+							</li>
+							<li>
+								<Link className="text-[#c4621b] hover:text-opacity-70 opacity-80 transition-colors" href="https://oranjetaxi.xyz">
 									oranjetaxi
 								</Link>{" "}
 								- a taxi company within the virtual experience De Antillen on Roblox.
@@ -45,10 +51,10 @@ export default function Home() {
 						<h3 className="text-lg font-semibold text-white">// experience</h3>
 						<ul className="list-disc pl-5 space-y-1 text-neutral-300">
 							<li>
-								<Link className="text-green-600 hover:text-green-700 opacity-80 transition-colors" href="https://www.serieswork.com">
-									frontend software engineer, marketing at sailboat games / series
+								<Link className="text-green-600 hover:text-opacity-70 opacity-80 transition-colors" href="https://www.serieswork.com">
+									senior frontend engineer at sailboat games / series
 								</Link>{" "}
-								- in charge of crafting and maintaining all landing pages for both Series and Eastside, two companies under the Sailboat Games umbrella.
+								- in charge of crafting and maintaining all frontend sites for both Series and Eastside, two companies under the Sailboat Games umbrella.
 							</li>
 						</ul>
 					</div>
@@ -57,19 +63,19 @@ export default function Home() {
 						<ul className="list-disc pl-5 space-y-1 text-neutral-300">
 							<li>
 								email -{" "}
-								<Link className="text-neutral-500 hover:text-neutral-300 opacity-80 transition-colors" href="mailto:me@colenh.dev">
+								<Link className="text-neutral-500 hover:text-opacity-70 opacity-80 transition-colors" href="mailto:me@colenh.dev">
 									me@colenh.dev
 								</Link>
 							</li>
 							<li>
 								github -{" "}
-								<Link className="text-neutral-500 hover:text-neutral-300 opacity-80 transition-colors" href="https://github.com/colenh">
+								<Link className="text-neutral-500 hover:text-opacity-70 opacity-80 transition-colors" href="https://github.com/colenh">
 									@colenh
 								</Link>
 							</li>
 							<li>
 								read.cv -{" "}
-								<Link className="text-neutral-500 hover:text-neutral-300 opacity-80 transition-colors" href="https://read.cv/colenh">
+								<Link className="text-neutral-500 hover:text-opacity-70 opacity-80 transition-colors" href="https://read.cv/colenh">
 									@colenh
 								</Link>
 							</li>
@@ -78,7 +84,7 @@ export default function Home() {
 					<div className="text-neutral-300">
 						<p>
 							you can find the source code for this website on github at{" "}
-							<Link className="text-neutral-500 hover:text-neutral-300 opacity-80 transition-colors" href="https://github.com/colenh/portfolio">
+							<Link className="text-neutral-500 hover:text-opacity-70 opacity-80 transition-colors" href="https://github.com/colenh/portfolio">
 								colenh/portfolio
 							</Link>
 							.
