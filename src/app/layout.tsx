5d84ca343dc05cf6dcb52ceffa00c6a6c9479b16
colenh <me@colenh.dev>
2025-10-28 20:19:52 -0400

    update deps; fix page layout background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import { cn } from "@/lib/utils";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -33,7 +34,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>{children}</body>
+			<body className={cn("bg-neutral-900 text-neutral-300", inter.className)}>{children}</body>
 		</html>
 	);
 }
